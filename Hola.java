057323e63ca66e4876c7d1c3a2f0640f9dee7caa
Alejandro <amuse@campusaula.com>
2013-09-23 13:19:09 +0200

    He añadido la clase Scanner

--- Hola.java
+++ Hola.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 public class Hola {
 
   public static void main (String args[]){
-  
+
+  Scanner teclado = new Scanner(System.in);  
   System.out.println("Hola mundo");
   }
   
